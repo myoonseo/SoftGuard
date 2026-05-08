b76ca55695673634cbee83f00029e2e3fb107995
myoonseo <choiys613@gmail.com>
2026-05-08 14:21:54 +0900

    remove unfinished entity

--- src/main/java/com/example/softguard/domain/Event_summary.java
+++ src/main/java/com/example/softguard/domain/Event_summary.java
@@ -1,10 +1,10 @@
-package com.example.softguard.domain;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Event_summary {
-    //최신 요약 
-    // 운영제안
-    // 사고전환율
-}
+//package com.example.softguard.domain;
+//
+//import jakarta.persistence.Entity;
+//
+//@Entity
+//public class Event_summary {
+//    //최신 요약
+//    // 운영제안
+//    // 사고전환율
+//}
